d5c70ee38f0217e20e0039a6c71f762aa0a5d3db
muntasiir <152467761+muntasiir@users.noreply.github.com>
2024-06-06 00:25:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -232,4 +232,4 @@ def load_user(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5004)  # Changed the port number to not collide with 4.0 & 5.0
+    app.run(host='0.0.0.0', port=80)  # Listen on all available network interfaces on port 80
